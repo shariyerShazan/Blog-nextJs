2c3c4fdb49d5c0c81f29976220103b20ef581468
shariyerShazan <shariyershazan1@gmail.com>
2025-09-13 05:19:03 +0600

    modeToggle theme

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ModeToggle } from './ModeToggle'
+
+const Navbar = () => {
+  return (
+    <div>
+         <ModeToggle />
+    </div>
+  )
+}
+
+export default Navbar
